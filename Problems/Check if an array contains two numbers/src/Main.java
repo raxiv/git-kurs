26bbfac4049618516e1f090da81e9877718bd2ba
Dawid <dawidj119@interia.eu>
2021-10-21 20:56:04 +0200

    sprawdz czy tablica zawiera 2 liczby

--- Problems/Check if an array contains two numbers/src/Main.java
+++ Problems/Check if an array contains two numbers/src/Main.java
@@ -1,8 +1,28 @@
 import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
 
 class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i <array.length ; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        boolean flag = false;
+
+        for (int i = 0; i <array.length-1 ; i++) {
+            if((array[i] == n && array[i+1]==m) || (array[i] == m && array[i+1]==n) ){
+                flag = true;
+            }
+        }
+        System.out.println(" " + flag);
     }
 }
